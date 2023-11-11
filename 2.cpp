38c2cfecd674ff193175f848b8ddac858a24f40b
Andrey Bagryanskiy <andrey@1dist.ru>
2023-11-12 00:26:47 +0300

    2

---
+++ 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include <memory>
+
+
+int main(){
+    std::vector<int>  inVec{1,2,3,4,5,1,8,16,4,19,19};
+    
+    auto getUnique =[](const std::vector<int>& vec)
+    {
+        std::unordered_multiset<int> set(vec.begin(), vec.end());
+        auto result = std::make_unique<std::vector<int>>();
+        for(auto i: set){
+            if(set.count(i) == 1)
+            result->push_back(i);
+        }
+        return result;
+    };
+    
+    auto getVec = getUnique(inVec);
+    for(auto i: *getVec){
+        std::cout << i << " ";
+    }
+
+}
