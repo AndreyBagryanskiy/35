fb2e2d892e1195e0af6477df9236e992abd062d1
Andrey Bagryanskiy <Andrey@1dist.ru>
2023-11-14 09:31:47 +0300

    1.01

--- 3.cpp
+++ 3.cpp
@@ -5,7 +5,26 @@
 
 
 int main(){
-
-
-    
+    auto getFilesWithExtension = [](const std::filesystem::path& path, const std::string& extension){
+        auto fileNames = std::make_unique<std::vector<std::filesystem::path>>();
+        for(auto& p: std::filesystem::recursive_directory_iterator(path)){
+            if (p.is_regular_file()){
+                if (p.path().extension().compare(extension) == 0){
+                    fileNames->push_back(p.path().filename());
+                }
+            }
+        }
+        return fileNames;
+    };
+    std::filesystem::path path = "test";
+    std::string extension = ".txt";
+    try{
+        auto fileNames = getFilesWithExtension(path, extension);
+        for(auto i: *fileNames){
+        std::cout << i << " ";
+        }
+    }
+    catch (const std::exception& e){
+        std::cerr << e.what();
+    } 
 }
